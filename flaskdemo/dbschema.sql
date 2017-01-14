a2c8f28cb93cbb32673f20cf52be7d0870c1dc7b
mrbarge <matt.bargenquast@gmail.com>
2017-01-14 15:27:26 +1100

    Corrected fk constraint in dump and refactored table names.

--- flaskdemo/dbschema.sql
+++ flaskdemo/dbschema.sql
@@ -1,13 +1,13 @@
-DROP TABLE IF EXISTS books;
-DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS review;
 
-CREATE TABLE books (id INTEGER PRIMARY KEY, name TEXT NOT NULL, release_date DATETIME);
-CREATE TABLE reviews (id INTEGER, book_id INTEGER, review TEXT, rating INTEGER, PRIMARY KEY (id, book_id) );
+CREATE TABLE book (id INTEGER PRIMARY KEY, name TEXT NOT NULL, release_date DATETIME);
+CREATE TABLE review (id INTEGER, book_id INTEGER, review TEXT, rating INTEGER, PRIMARY KEY (id), FOREIGN KEY (book_id) REFERENCES book(id));
 
-INSERT INTO books (name, release_date) VALUES ('Book1',CURRENT_TIMESTAMP);
-INSERT INTO books (name, release_date) VALUES ('Book2',CURRENT_TIMESTAMP);
-INSERT INTO reviews (book_id, review, rating) VALUES (1,'I did not like this book.',2);
-INSERT INTO reviews (book_id, review, rating) VALUES (1,'I loved this book.',5);
-INSERT INTO reviews (book_id, review, rating) VALUES (2,'What a weird book.',3);
+INSERT INTO book (id, name, release_date) VALUES (1, 'Book1',CURRENT_TIMESTAMP);
+INSERT INTO book (id, name, release_date) VALUES (2, 'Book2',CURRENT_TIMESTAMP);
+INSERT INTO review (id, book_id, review, rating) VALUES (1,1,'I did not like this book.',2);
+INSERT INTO review (id, book_id, review, rating) VALUES (2,1,'I loved this book.',5);
+INSERT INTO review (id, book_id, review, rating) VALUES (3,2,'What a weird book.',3);
 
 
